9a628f7cdcff548171fd6d126771f43973839c90
CHUKIN <dmitriyatamanchuk@gmail.com>
2017-04-03 10:17:14 +0300

    Complete B

--- src/by/it/group473601/atamanchuk/lesson07/B_EditDist.java
+++ src/by/it/group473601/atamanchuk/lesson07/B_EditDist.java
@@ -40,14 +40,34 @@ import java.util.Scanner;
 public class B_EditDist {
 
 
-    int getDistanceEdinting(String one, String two) {
+    int getDistanceEdinting(String S1, String S2) {
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
+        int m = S1.length(), n = S2.length();
+        int[] D1;
+        int[] D2 = new int[n + 1];
 
+        for(int i = 0; i <= n; i ++)
+            D2[i] = i;
 
-        int result = 0;
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        for(int i = 1; i <= m; i ++) {
+            D1 = D2;
+            D2 = new int[n + 1];
+            for(int j = 0; j <= n; j ++) {
+                if(j == 0) D2[j] = i;
+                else {
+                    int cost = (S1.charAt(i - 1) != S2.charAt(j - 1)) ? 1 : 0;
+                    if(D2[j - 1] < D1[j] && D2[j - 1] < D1[j - 1] + cost)
+                        D2[j] = D2[j - 1] + 1;
+                    else if(D1[j] < D1[j - 1] + cost)
+                        D2[j] = D1[j] + 1;
+                    else
+                        D2[j] = D1[j - 1] + cost;
+                }
+            }
+        }
+        return D2[n];
+
     }
 
 
@@ -55,7 +75,7 @@ public class B_EditDist {
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/473601/atamanchuk/lesson08/dataABC.txt");
+        InputStream stream = new FileInputStream(root + "by/it/group473601/atamanchuk/lesson07/dataABC.txt");
         B_EditDist instance = new B_EditDist();
         Scanner scanner = new Scanner(stream);
         System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
